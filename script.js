c97c845e4ed8dbb231543bee8827639e6c42ca66
Antonio Emmanuel Villarreal <emmanuelvillarreal369@gmail.com>
2023-05-10 11:54:01 -0500

    Js working, needs spacing html elements

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-const parentNode = document.getElementById('div-main')
-const element = parentNode.querySelector('.p')
-const newElement = element.innerHTML =  'uuo'
-console.log(element);
+"use strict"
+
+// Nested object in array for data manipulation
+ const quoteData = [
+  {author: 'Walt Disney',
+  quote: 'The way to get started is to quit talking and begin doing.'},
+  {author: 'Jim Carry',
+  quote: 'Behind every great man is a woman rolling her eyes.'},
+  {author: 'Mark Twain',
+  quote: 'To succeed in life, you need two things: ignorance and confidence'},
+  {author: 'Frank Sinatra',
+  quote: 'The best revenge is massive success.'},
+  {author: 'Abraham Lincoln',
+  quote: 'I walk slowly but never backwards.'}
+]
+
+//Dom element selection
+const genButton = document.body.querySelector('button')
+
+//Gen button  Function
+genButton.addEventListener("click", function() {
+  const rdmNum = (Math.floor(Math.random() * 4));
+  const arrayindx = quoteData[rdmNum]
+  const authorObj = arrayindx.author
+  const quotObj = arrayindx.quote
+  const authorBox  = document.body.querySelector('.author-txt')
+  authorBox.innerText = `${authorObj}`
+  const quoteBox = document.body.querySelector('p')
+  quoteBox.innerText = `${quotObj}`
+  return console.log(authorObj)
+})
